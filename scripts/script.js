c0e574ffd993059c7f27ddd1a88d5d2f4d9e8008
taranamol <tarskaur@gmail.com>
2016-10-17 16:10:18 -0700

    with black & white added

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
 
-})
+   document.getElementById("navMenu").innerHTML =
+    '<nav class="navbar navbar-default navbar-fixed-top">'+
+    '<div class="container-fluid">'+
+    '<div class="navbar-header">'+
+    '<a class="navbar-brand" href="index.html">taranamol kaur </a>'+
+     '</div>'+
+    '<ul class="nav navbar-nav navbar-right">'+
+    '<li><a href="designindex.html" role="button" aria-haspopup="true" aria-expanded="false">designer<span></span></a></li>'+
+     '<li><a href="devindex.html" role="button" aria-haspopup="true" aria-expanded="false">developer<span></span></a></li>'+
+    '<li><a href="writer.html" role="button" aria-haspopup="true" aria-expanded="false">writings<span></span></a></li>'+
+    '<li><a href="pdfs/kaur_resume.pdf" target="_blank" role="button" aria-haspopup="true" aria-expanded="false">resume<span></span></a></a></li>'+
+     '</div>'+
+    '</div>'+
+    '</nav>';
+
+   document.getElementById("footerMenu").innerHTML =
+    '<div class="contactpage">'+
+    '<div id="footer">'+
+    '<div class="container">'+
+    '<h2> contact taranamol | '+
+    '<a href="mailto:tarskaur@gmail.com" target="_blank"> email </a> |'+
+    '<a href="https://www.linkedin.com/in/taranamolkaur" target="_blank"> linkedin </a> |'+
+    '<a href="https://github.com/taranamol" target="_blank"> github</a> </h2>'+
+    '</div>'+
+    '</div>'+
+    '</div>';
+
+});
