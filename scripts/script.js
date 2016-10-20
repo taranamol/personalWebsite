c2833c91839ccf958b354b39b31eb4bcb031cc96
taranamol <tarskaur@gmail.com>
2016-10-20 14:51:07 -0700

    adobe blog up

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,25 @@
 $(document).ready(function(){
 
    document.getElementById("navMenu").innerHTML =
-    '<nav class="navbar navbar-default navbar-fixed-top">'+
+    '<nav class="navbar navbar-default navbar-fixed-top"'+
     '<div class="container-fluid">'+
     '<div class="navbar-header">'+
-    '<a class="navbar-brand" href="index.html">taranamol kaur </a>'+
-     '</div>'+
-    '<ul class="nav navbar-nav navbar-right">'+
-    '<li><a href="designindex.html" role="button" aria-haspopup="true" aria-expanded="false">designer<span></span></a></li>'+
-     '<li><a href="devindex.html" role="button" aria-haspopup="true" aria-expanded="false">developer<span></span></a></li>'+
-    '<li><a href="writer.html" role="button" aria-haspopup="true" aria-expanded="false">writings<span></span></a></li>'+
-    '<li><a href="pdfs/kaur_resume.pdf" target="_blank" role="button" aria-haspopup="true" aria-expanded="false">resume<span></span></a></a></li>'+
-     '</div>'+
+    '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">'+
+    '<span class="sr-only">Toggle navigation</span>'+
+    '<span class="icon-bar"></span>'+
+    '<span class="icon-bar"></span>'+
+    '<span class="icon-bar"></span>'+
+    '</button>'+
+    '<a class="navbar-brand" href="index.html">taranamol kaur</a>'+
+    '</div>'+
+    '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">' + 
+    '<ul class="nav navbar-nav navbar-right">' + 
+    '<li><a href="designindex.html">designer</a></li>' + 
+    '<li><a href="devindex.html">developer</a></li>' + 
+    '<li><a href="writer.html">writings</a></li>' + 
+    '<li><a href="pdfs/kaur_resume.pdf" target="_blank">resume</a></li>' + 
+    '</ul>' + 
+    '</div>'+
     '</div>'+
     '</nav>';
 
