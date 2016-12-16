5bc7f120ed018e1c0dba16bbb2c748183350fa46
taranamol <tarskaur@gmail.com>
2016-12-15 22:14:23 -0800

    newlookwithbg

--- scripts/script.js
+++ scripts/script.js
@@ -14,8 +14,8 @@ $(document).ready(function(){
     '</div>'+
     '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">' + 
     '<ul class="nav navbar-nav navbar-right">' + 
-    '<li><a href="designindex.html">designer</a></li>' + 
-    '<li><a href="devindex.html">developer</a></li>' + 
+    '<li><a href="designindex.html">projects</a></li>' + 
+    // '<li><a href="devindex.html">developer</a></li>' + 
     '<li><a href="writer.html">writings</a></li>' + 
     '<li><a href="pdfs/kaur_resume.pdf" target="_blank">resume</a></li>' + 
     '</ul>' + 
